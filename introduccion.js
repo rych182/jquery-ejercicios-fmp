e155308f746ade9fc579522beb3bad91a1da8d09
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-02-08 02:06:13 -0600

    sintaxis de jquery

---
+++ introduccion.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	//alert("");
+	$('h1').hide();
+})
+
+/*
+		Explicación del código
+Cuando el documento (introduccion.html), este listo(ready),
+me vas a ejecutar la funcion alert
+*/
+
+
+/*
+		Sintaxis de Jquery
+$  -        Indicamos que vamos acceder a jquery
+selector -  Accedemos al elemento que queramos trabajar.
+accion - lo que queremos hacer con el elemento seleccionado
+
+
+$ + ('elemento') + .accion();
+*/
