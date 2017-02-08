6e8a7fd42cb87392c665122ccf77ca29c314cd4e
Ricardo Garrido <ricgarridoc@hotmail.com>
2017-02-08 02:33:22 -0600

    primeros ejercicios de jquery

---
+++ selectores.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	//alert();
+	//$("h1, h2").hide(); 		etiquetas
+	//$(".primero").hide();		clases de CSS
+	//$("#primero").hide();		id de html
+	//$(".primero a").hide(); 	oculta la etiqueta a dentro de la clase .primero
+});
+
+/*
+1-El alert carga después de que se cargue todo el archivo html
+
+2-Primero va la libreria jquery y después el archivo,
+en ese orden.
+*/
+
